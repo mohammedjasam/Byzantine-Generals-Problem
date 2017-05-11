b1631c07f9206735178883bc3e6285d5834bb00c
Mohammed Jasam <mnqnd@mst.edu>
2017-05-11 15:25:47 -0500

    Working on Multiple clients

--- Python/Client.py
+++ Python/Client.py
@@ -1,23 +1,41 @@
-import socket
+#!usr/bin/python
+from socket import *
 
-def main():
-    host = "127.0.0.1"
-    port = 5000
+host = 'localhost' # '127.0.0.1' can also be used
+port = 52000
 
-    s = socket.socket()
-    s.connect((host, port))
+sock = socket()
+#Connecting to socket
+sock.connect((host, port)) #Connect takes tuple of host and port
 
-    message = raw_input("-> ")
+#Infinite loop to keep client running.
+while True:
+    data = sock.recv(1024)
+    print data
+    sock.send('HI! I am client.')
 
-    while message!="q":
-        s.sendall(message)
-        data = s.recv(1024)
+sock.close()
 
-        print "Received from Server: "+ str(data)
-
-        message = raw_input("-> ")
-
-    s.close()
-
-if __name__ == "__main__":
-    main()
+# import socket
+#
+# def main():
+#     host = "127.0.0.1"
+#     port = 5000
+#
+#     s = socket.socket()
+#     s.connect((host, port))
+#
+#     message = raw_input("-> ")
+#
+#     while message!="q":
+#         s.sendall(message)
+#         data = s.recv(1024)
+#
+#         print "Received from Server: "+ str(data)
+#
+#         message = raw_input("-> ")
+#
+#     s.close()
+#
+# if __name__ == "__main__":
+#     main()
--- Python/Server.py
+++ Python/Server.py
@@ -1,29 +1,64 @@
-import socket
+# Import all from module socket
+from socket import *
+#Importing all from thread
+from thread import *
+
+# Defining server address and port
+host = ''  #'localhost' or '127.0.0.1' or '' are all same
+port = 52000 #Use port > 1024, below it all are reserved
+
+#Creating socket object
+sock = socket()
+#Binding socket to a address. bind() takes tuple of host and port.
+sock.bind((host, port))
+#Listening at the address
+sock.listen(5) #5 denotes the number of clients can queue
+
+def clientthread(conn):
+#infinite loop so that function do not terminate and thread do not end.
+     while True:
+#Sending message to connected client
+         conn.send('Hi! I am server\n') #send only takes string
+#Receiving from client
+         data = conn.recv(1024) # 1024 stands for bytes of data to be received
+         print data
+
+while True:
+#Accepting incoming connections
+    conn, addr = sock.accept()
+#Creating new thread. Calling clientthread function for this function and passing conn as argument.
+    start_new_thread(clientthread,(conn,)) #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+
+conn.close()
+sock.close()
 
 
-def main():
-    # Defining the Host address and the port at which we want to communicate
-    host = ""
-    port = 5000
-
-    s = socket.socket()
-
-    # binding the socket to the host and port
-    s.bind((host, port))
-
-    # We listen to one Client at a time!
-    s.listen(1)
-    c, addr = s.accept()
-    print "Connection From: " + str(addr)
-    while True:
-        data  =  c.recv(1024)
-        if not data:
-            break
-        print "From connected user: " + str(data)
-        data = str(data).upper()
-        print "Sending: " + str(data)
-        c.send(data)
-
-    c.close()
-if __name__ == "__main__":
-    main()
+# import socket
+#
+#
+# def main():
+#     # Defining the Host address and the port at which we want to communicate
+#     host = ""
+#     port = 5000
+#
+#     s = socket.socket()
+#
+#     # binding the socket to the host and port
+#     s.bind((host, port))
+#
+#     # We listen to one Client at a time!
+#     s.listen(7)
+#     c, addr = s.accept()
+#     print "Connection From: " + str(addr)
+#     while True:
+#         data  =  c.recv(10)
+#         if not data:
+#             break
+#         print "From connected user: " + str(data)
+#         data = str(data).upper()
+#         print "Sending: " + str(data)
+#         c.send(data)
+#
+#     c.close()
+# if __name__ == "__main__":
+#     main()
