c9ebf0bb68227b6f187bb985b79c34003b8669a0
Mohammed Jasam <mnqnd@mst.edu>
2017-05-11 13:02:48 -0500

    Server is done

--- Python/Server.py
+++ Python/Server.py
@@ -1,11 +1,29 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sock.bind(("127.0.0.1",8000))
+def main():
+    # Defining the Host address and the port at which we want to communicate
+    host = "127.0.0.1"
+    port = 5000
 
-sock.listen(2)
+    s = socket.socket()
 
-(client,(ip,port)) = sock.accept()
+    # binding the socket to the host and port
+    s.bind((host, port))
 
-client.send("HI")
+    # We listen to one Client at a time!
+    s.listen(1)
+    c, addr = s.accept()
+    print("Connection From: " + str(addr))
+    while True:
+        data  =  c.recv(1024)
+        if not data:
+            break
+        print("From connected user: " + str(data))
+        data = str(data).upper()
+        print("Sending: " + str(data))
+        c.send(data)
+
+    c.close()
+if __name__ = "__main__":
+    main()
