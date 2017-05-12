d402a978c28a3370af5e2b7ca20281feb07e61ff
Mohammed Jasam <mnqnd@mst.edu>
2017-05-12 00:49:12 -0500

    Finally Works

--- Python/Client.py
+++ Python/Client.py
@@ -7,17 +7,18 @@ host = 'localhost' # '127.0.0.1' can also be used
 port = 52000
 
 index,noL,noT,command = -1,-1,-1,-1
+
 isT = -1
 sock = socket()
 sock.connect((host, port)) #Connect takes tuple of host and port
 
 noOP = []
 noIP = []
-
+last = -1
 while 1:
     data = sock.recv(1024)
     print data
-    print "\n"
+    # print "\n"
 
     dataArr = data.split()
     keyWord = dataArr[0]
@@ -38,73 +39,45 @@ while 1:
 
                 if isT==1:
                     random = randint(0,1)
-                    # print random
+                    time.sleep(0.5)
                     sock.send("INPUT C|%s %s %s" %(index,random,j))
                 else:
+                    time.sleep(0.5)
                     sock.send("INPUT C|%s %s %s" %(index,command,j))
 
 
     elif keyWord == "INPUT":
+        # print "dataArr is: %s" %dataArr[1]
         noIP.append(dataArr[1])
         if len(noIP) == noL-1:
             for val in noIP:
                 path = val.split("|")
                 last = path[len(path)-1]
-                print last
-                time.sleep(0.5)
+                # print last
                 if isT:
+                    time.sleep(0.5)
                     sock.send("OUTPUT %s %s" %(randint(0,1),last))
                 else:
+                    time.sleep(0.5)
                     sock.send("OUTPUT %s %s" %(command,last))
 
     elif keyWord == "OUTPUT":
+        # try:
+        #     if last!=-1:
+
+        print "DataArr is: %s"%dataArr
         noOP.append(dataArr[1])
+        # print "OP is: %s" %noOP
         if len(noOP) == noL-1:
+            time.sleep(5)
             if noOP.count("1")>noOP.count("0"):
+                # time.sleep(1)
                 sock.send("MAJORITY 1")
                 print "MAJORITY 1"
-            else:
+            elif noOP.count("1")<noOP.count("0"):
+                # time.sleep(1)
                 sock.send("MAJORITY 0")
                 print "MAJORITY 0"
-
-
-
-sock.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import socket
-#
-# def main():
-#     host = "127.0.0.1"
-#     port = 5000
-#
-#     s = socket.socket()
-#     s.connect((host, port))
-#
-#     message = raw_input("-> ")
-#
-#     while message!="q":
-#         s.sendall(message)
-#         data = s.recv(1024)
-#
-#         print "Received from Server: "+ str(data)
-#
-#         message = raw_input("-> ")
-#
-#     s.close()
-#
-# if __name__ == "__main__":
-#     main()
+            else:
+                sock.send("MAJORITY %s"%)
+                print "MAJORITY %s" %s
--- Python/Server.py
+++ Python/Server.py
@@ -4,6 +4,8 @@ from thread import *
 import math
 from random import randint
 
+GlobalReq = 0
+q={}
 
 # Defining server address and port
 host = ''  #'localhost' or '127.0.0.1' or '' are all same
@@ -13,7 +15,9 @@ port = 52000 #Use port > 1024, below it all are reserved
 
 # print "Enter"
 print "# of L, # of T, isTraitor?, Command\n"
-noL,noT,isT,command = 3,1,1,1
+s=raw_input().split()
+s=[int(x) for x in s]
+noL,noT,isT,command = s[0],s[1],s[2],s[3]
 noM = []
 
 ran = []  ## List to store the Random Traitors
@@ -49,6 +53,7 @@ def clientthread(conn):
              child.send("INPUT %s %s" %(path, cmd))
 
          if keyWord == "OUTPUT":
+            #  print "dataArr is: %s"%dataArr
              cmd = dataArr[1]
              child = connList[int(dataArr[2])]
              child.send("OUTPUT %s" %(cmd))
@@ -71,64 +76,33 @@ while True:
     if len(connList)==noL:
         break
 
-print "first"
+# print "first"
 for x in range(noL):
     connList[x].send("INDEX %s"%x)
-    time.sleep(0.05)
+    time.sleep(0.5)
 
 
-print "second"
+# print "second"
 for i in ran:
     connList[i].send("TRAITOR %s"%i)
-    time.sleep(0.05)
+    time.sleep(0.5)
 
 
 #starting algorithm
-print "third"
+# print "third"
 if isT:
     for x in range(noL):
+        time.sleep(1)
+
         connList[x].send("COMMAND %s %s C %s" %(noL, noT, randint(0,1)))
-        time.sleep(0.05)
 else:
     for x in range(noL):
+        time.sleep(1)
+
         connList[x].send("COMMAND %s %s C %s" %(noL, noT, command))
-        time.sleep(0.05)
+
+
+
 
 while 1:
     a=1
-
-
-
-# conn.close()
-# sock.close()
-
-
-# import socket
-#
-#
-# def main():
-#     # Defining the Host address and the port at which we want to communicate
-#     host = ""
-#     port = 5000
-#
-#     s = socket.socket()
-#
-#     # binding the socket to the host and port
-#     s.bind((host, port))
-#
-#     # We listen to one Client at a time!
-#     s.listen(7)
-#     c, addr = s.accept()
-#     print "Connection From: " + str(addr)
-#     while True:
-#         data  =  c.recv(10)
-#         if not data:
-#             break
-#         print "From connected user: " + str(data)
-#         data = str(data).upper()
-#         print "Sending: " + str(data)
-#         c.send(data)
-#
-#     c.close()
-# if __name__ == "__main__":
-#     main()
